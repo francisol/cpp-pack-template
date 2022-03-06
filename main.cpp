1604665b3f20a6dcf117d177258993220697dc93
Xie Zhongtao <xie_zhongtao@163.com>
2022-03-06 13:04:48 +0800

    init

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <cstdarg>
+#include <cstdint>
+#include <cstdlib>
+#include <ostream>
+#include <new>
+
+extern "C" {
+
+int main_run();
+
+}
+
+int main(){
+    return main_run();
+}
---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+#[no_mangle]
+pub extern "C" fn main_run()-> i32 {
+    println!("Hello, world!");
+    return 0;
+}
